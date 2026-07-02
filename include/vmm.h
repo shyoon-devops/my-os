3093f734b1e0ba27016218b1df6567f33adf9354
sh.yoon <121743032+shyoon-devops@users.noreply.github.com>
2026-07-02 18:12:37 +0900

    [phase-10e] Avoid PTE macro redefinition

--- include/vmm.h
+++ include/vmm.h
@@ -3,15 +3,15 @@
 
 #include "types.h"
 
-#define PTE_PRESENT  0x001ULL
-#define PTE_WRITABLE 0x002ULL
-#define PTE_USER     0x004ULL
-#define PTE_HUGE     0x080ULL
+static const u64 PTE_PRESENT  = 0x001ULL;
+static const u64 PTE_WRITABLE = 0x002ULL;
+static const u64 PTE_USER     = 0x004ULL;
+static const u64 PTE_HUGE     = 0x080ULL;
 
-#define PTE_ADDR_MASK 0x000FFFFFFFFFF000ULL
+static const u64 PTE_ADDR_MASK = 0x000FFFFFFFFFF000ULL;
 
-#define VMM_TEST_VIRT        0x0000000040000000ULL
-#define VMM_UNMAP_TEST_VIRT  0x0000000040001000ULL
+static const u64 VMM_TEST_VIRT       = 0x0000000040000000ULL;
+static const u64 VMM_UNMAP_TEST_VIRT = 0x0000000040001000ULL;
 
 void vmm_map_page(u64 virt_addr, u64 phys_addr, u64 flags);
 u64 vmm_unmap_page(u64 virt_addr, u32 free_phys);

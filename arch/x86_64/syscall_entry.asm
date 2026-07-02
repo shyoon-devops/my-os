16bde656758cb2ef2000108bc38b8771e8eb1825
sh.yoon <121743032+shyoon-devops@users.noreply.github.com>
2026-07-02 19:56:25 +0900

    [phase-10e] Branch SYS_exit before syscall dispatch

--- arch/x86_64/syscall_entry.asm
+++ arch/x86_64/syscall_entry.asm
@@ -30,14 +30,20 @@ syscall_entry:
     push rcx
     push r11
 
-    ; syscall_dispatch() 호출 뒤에도 원래 syscall number가 필요하다.
-    ; r12는 System V ABI callee-saved register라서 C 함수 호출 뒤에도 유지된다.
-    push r12
-    mov r12, rax
+    ; Phase 10-C/10-E에서는 SYS_exit만 kernel shell 복귀 신호다.
+    ; SYS_write 같은 일반 syscall은 dispatch 후 sysretq로 userland에 복귀한다.
+    cmp rax, 60
+    je syscall_dispatch_exit
 
+syscall_dispatch_return_user:
     ; syscall_dispatch(number, arg0, arg1, arg2, arg3, arg4, arg5)
     ; C ABI:
     ;   rdi, rsi, rdx, rcx, r8, r9, stack
+    ;
+    ; syscall 진입 시 user rsp는 16-byte aligned다.
+    ; rcx/r11 push 이후에도 16-byte aligned이므로,
+    ; 7번째 인자 push 전에 padding 8 byte를 넣어 call alignment를 맞춘다.
+    sub rsp, 8
     push r9          ; arg5, 7번째 C 인자
 
     mov r9, r8       ; arg4
@@ -49,20 +55,28 @@ syscall_entry:
 
     call syscall_dispatch
 
-    add rsp, 8       ; arg5 제거
+    add rsp, 16      ; arg5 + alignment padding 제거
 
-    ; Phase 10-C/10-E에서는 SYS_exit만 kernel shell 복귀 신호다.
-    ; SYS_write 같은 일반 syscall은 sysretq로 userland에 복귀해야 한다.
-    cmp r12, 60
-    je syscall_exit_to_kernel_shell
-
-    pop r12
     pop r11
     pop rcx
 
     sysretq
 
-syscall_exit_to_kernel_shell:
+syscall_dispatch_exit:
+    sub rsp, 8
+    push r9          ; arg5, 7번째 C 인자
+
+    mov r9, r8       ; arg4
+    mov r8, r10      ; arg3
+    mov rcx, rdx     ; arg2
+    mov rdx, rsi     ; arg1
+    mov rsi, rdi     ; arg0
+    mov rdi, rax     ; syscall number
+
+    call syscall_dispatch
+
+    add rsp, 16      ; arg5 + alignment padding 제거
+
     ; 현재 rsp는 ring3 user stack이다.
     ; ring3_enter()가 저장해둔 kernel stack으로 돌아간다.
     ; rax에는 syscall_dispatch(SYS_exit)의 반환값, 즉 exit code가 있다.
